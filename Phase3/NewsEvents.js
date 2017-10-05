2cbd45403bbfda497e8aafedb9ab5b1c1a763eb1
oochitecht <plorentz7@gmail.com>
2017-10-05 12:17:37 -0500

    Opportunities in Chemistry page started. News/Events element made into an include. Clubs page should be done.

---
+++ Phase3/NewsEvents.js
@@ -0,0 +1,75 @@
+//This simply write the html for the navbar
+//this is used only as a workaround to import html to html
+document.write('\
+<link rel="stylesheet" href="NewsEvents.css">\
+<section>\
+	<h3>News / Events</h3>\
+	<div class="calender">\
+		<div class="month">\
+		  <ul>\
+		    <li class="prev">&#10094;</li>\
+		    <li class="next">&#10095;</li>\
+		    <li>\
+		      January<br>\
+		      <span class="year">2017</span>\
+		    </li>\
+		  </ul>\
+		</div>\
+		<ul class="weekdays">\
+		  <li>Mo</li>\
+		  <li>Tu</li>\
+		  <li>We</li>\
+		  <li>Th</li>\
+		  <li>Fr</li>\
+		  <li>Sa</li>\
+		  <li>Su</li>\
+		</ul>\
+		<ul class="days">\
+		  <li>1</li>\
+		  <li>2</li>\
+		  <li>3</li>\
+		  <li>4</li>\
+		  <li>5</li>\
+		  <li>6</li>\
+		  <li>7</li>\
+		  <li>8</li>\
+		  <li>9</li>\
+		  <li><span class="active">10</span></li>\
+		  <li>11</li>\
+		  <li>12</li>\
+		  <li>13</li>\
+		  <li>14</li>\
+		  <li>15</li>\
+		  <li>16</li>\
+		  <li>17</li>\
+		  <li>18</li>\
+		  <li>19</li>\
+		  <li>20</li>\
+		  <li>21</li>\
+		  <li>22</li>\
+		  <li>23</li>\
+		  <li>24</li>\
+		  <li>25</li>\
+		  <li>26</li>\
+		  <li>27</li>\
+		  <li>28</li>\
+		  <li>29</li>\
+		  <li>30</li>\
+		  <li>31</li>\
+		</ul>\
+	</div>\
+</section>\
+<div class="events">\
+	<h3>January 10th</h3>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+	<ul>Meeting at the MUC at 9:00 am</ul>\
+</div>\
+');
